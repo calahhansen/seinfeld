774f486aab796b95e77c211895fcae81b0219228
Calah Hansen <52944732+calahhansen@users.noreply.github.com>
2019-09-19 14:07:20 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+// Dependencies
+const express = require("express");
+const mysql = require("mysql");
+
+// Create express app instance.
+const app = express();
+
+// Set the port of our application
+// process.env.PORT lets the port be (this is the server port)
+const PORT = process.env.PORT || 8080;
+
+// MySQL DB Connection Information (remember to change this with our specific credentials)
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 8889, //(mysql port)
+  user: "root",
+  password: "root",
+  database: "show_db"
+});
+
+// Initiate MySQL Connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
---
+++ showDB.sql
@@ -0,0 +1,21 @@
+-- Drops the programming_db if it already exists --
+DROP DATABASE IF EXISTS show_db;
+
+-- Created the DB "show_db" (only works on local connections)
+CREATE DATABASE show_db;
+
+-- Use the DB show_db for all the rest of the script
+USE show_db;
+
+-- Created the table "actors"
+CREATE TABLE actors (
+  "id" int AUTO_INCREMENT NOT NULL,
+  "name" varchar(30) NOT NULL,
+  "coolpts" integer(3) NOT NULL,
+  "attitude" varchar(16) NOT NULL,
+  PRIMARY KEY(id)
+);
+
+-- Inserted a set of records into the table
+INSERT INTO actors ("name", "coolpts", "attitude")
+VALUES ("Jerry", 90, "relaxed"), ("Elaine", 80, "righteous"), ("Kramer", 20, "doofus"), ("George", 70, "selfish");
