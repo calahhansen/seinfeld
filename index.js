4c7e3d8588c236d73979b6f4e28a25d908f1dbb7
Calah Hansen <52944732+calahhansen@users.noreply.github.com>
2019-09-19 14:23:13 -0500

    show DB created and 2 ports for server and sql

--- index.js
+++ index.js
@@ -32,3 +32,33 @@ app.listen(PORT, function() {
   // Log (server-side) when our server has started
   console.log("Server listening on: http://localhost:" + PORT);
 });
+
+// Routes
+app.get("/", function(req, res) {
+
+  // If the main route is hit, then we initiate a SQL query to grab all records.
+  // All of the resulting records are stored in the variable "result."
+  connection.query("SELECT * FROM actors", function(err, result) {
+    if (err) throw err;
+    // We then begin building out HTML elements for the page.
+    const html = "<h1> Seinfeld Actors </h1>";
+
+    // Here we begin an unordered list.
+    html += "<ul>";
+
+    // We then use the retrieved records from the database to populate our HTML file.
+    for (let i = 0; i < result.length; i++) {
+      html += "<li><p> ID: " + result[i].id + "</p>";
+      html += "<p>Name: " + result[i].name + " </p>
+      html += ""
+      </li>";
+    }
+
+    // We close our unordered list.
+    html += "</ul>";
+
+    // Finally we send the user the HTML file we dynamically created.
+    res.send(html);
+  });
+});
+
